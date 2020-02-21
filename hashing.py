f4245480a662e137c1bd3e86b30d11ef9d22ba73
anonimeishon <sebastianmperea@gmail.com>
2020-02-21 13:00:45 -0500

    template

--- hashing.py
+++ hashing.py
@@ -1,15 +1,4 @@
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
 import random
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./prueba.db'
-db = SQLAlchemy(app)
-
-class posthash(db.Model):
-    id = db.Column(db.String, primary_key=True)
-    def __repr__(self):
-        return '<posthash %r>' % self.id
 
 
 def hasheo():
@@ -19,10 +8,6 @@ def hasheo():
         x = x + str(random.randint(0,9))
     y = hash(x)
     return (y)
-print (hasheo())
 
-x = hasheo()
-elhash = posthash(id=x)
-db.session.add(elhash)
-db.session.commit()
+
 
