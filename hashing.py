ae46f149e1ad672d575a7829dd393066ea936185
anonimeishon <sebastianmperea@gmail.com>
2020-02-21 10:00:17 -0500

    Add files via upload
    added db

--- hashing.py
+++ hashing.py
@@ -1,4 +1,17 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 import random
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./prueba.db'
+db = SQLAlchemy(app)
+
+class posthash(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    def __repr__(self):
+        return '<posthash %r>' % self.id
+
+
 def hasheo():
     x = 'T000'
 
@@ -7,3 +20,9 @@ def hasheo():
     y = hash(x)
     return (y)
 print (hasheo())
+
+x = hasheo()
+elhash = posthash(id=x)
+db.session.add(elhash)
+db.session.commit()
+
--- web.py
+++ web.py
@@ -3,21 +3,26 @@ from flask_sqlalchemy import SQLAlchemy
 from hashing import hasheo
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////./prueba.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./prueba.db'
 db = SQLAlchemy(app)
 
 class posthash(db.Model):
-    id = db.Column(db.String(80), unique=True)
-    def __repr__(self):
-        return '<hashed %r>' % self.hashed
+    id = db.Column(db.String, primary_key=True)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/chain')
-def adioh():
-    return 'adioh'
+@app.route('/chain', methods=['GET', 'POST'])
+def writehash():
+    if request.method == 'POST':
+        elhash = posthash(id=hasheo())
+        db.session.add(elhash)
+        db.session.commit()
+        return '<h1>Added hash</h1>'
+    elif request.method == 'GET':
+        return(posthash.query.all())
+        
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
