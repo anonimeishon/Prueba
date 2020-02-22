fc75826a93a4237cc1d7c4a740a3505454a83b5a
Anonimeishon <sebastianmperea@gmail.com>
2020-02-22 17:59:30 -0500

    added templates

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from hashing import hasheo
 app = Flask(__name__)
@@ -8,6 +8,8 @@ db = SQLAlchemy(app)
 
 class posthash(db.Model):
     id = db.Column(db.String, primary_key=True)
+    def __repr__(self):
+        return '<posthash %r>' % self.id
 
 @app.route('/')
 def hello_world():
@@ -15,13 +17,18 @@ def hello_world():
 
 @app.route('/chain', methods=['GET', 'POST'])
 def writehash():
+
     if request.method == 'POST':
         elhash = posthash(id=hasheo())
         db.session.add(elhash)
         db.session.commit()
-        return '<h1>Added hash</h1>'
+        return(render_template('post.html'))
+        #return ('<h1>Added hash</h1><form action="/chain" method="GET"><input type="submit" value="Back"></form>')
     elif request.method == 'GET':
-        return(posthash.query.all())
+        return render_template('get.html')
+        #return('<form action="/chain" method="POST"><input type="submit" value="Hash"></form>', )
+        #<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+        #return(posthash.query.all())
         
 
 if __name__ == '__main__':
